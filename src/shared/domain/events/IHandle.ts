4498c6acbe45e0e99b37ba03dcfbc62815739cdb
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2022-11-14 23:37:10 -0300

    feat: add shared domain files

---
+++ src/shared/domain/events/IHandle.ts
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { IDomainEvent } from "./IDomainEvent";
+
+export interface IHandle<IDomainEvent> {
+  setupSubscriptions(): void;
+}
