52ba20bdfc828596bd23276f43aa9faa0c01f3d1
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2022-11-15 11:33:34 -0300

    feat: add user name value object

---
+++ src/modules/user/domain/value-objects/userName/userName.ts
@@ -0,0 +1,36 @@
+import { Guard } from "../../../../../shared/core/Guard";
+import { ValueObject } from "../../../../../shared/domain/ValueObject";
+import { Result } from "./../../../../../shared/core/Result";
+
+type UserNameProps = {
+  name: string;
+};
+
+export class UserName extends ValueObject<UserNameProps> {
+  public static maxLength = 20;
+  public static minLength = 5;
+
+  private constructor(props: UserNameProps) {
+    super(props);
+  }
+
+  get value(): string {
+    return this.props.name;
+  }
+
+  public static create(props: UserNameProps): Result<UserName> {
+    const usernameResult = Guard.againstNullOrUndefined(props.name, "username");
+    if (usernameResult.isFailure) {
+      return Result.fail<UserName>(usernameResult.getErrorValue());
+    }
+    const minLengthResult = Guard.againstAtLeast(this.minLength, props.name);
+    if (minLengthResult.isFailure) {
+      return Result.fail<UserName>(minLengthResult.getErrorValue());
+    }
+    const maxLengthResult = Guard.againstAtMost(this.maxLength, props.name);
+    if (maxLengthResult.isFailure) {
+      return Result.fail<UserName>(maxLengthResult.getErrorValue());
+    }
+    return Result.ok<UserName>(new UserName(props))
+  }
+}
