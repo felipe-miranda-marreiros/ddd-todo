e4562306c26a7ea2f430f1f139d25c47ca0e9ed7
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2022-11-18 19:26:25 -0300

    feat: guard email against empty field

--- src/modules/user/domain/value-objects/userEmail/userEmail.ts
+++ src/modules/user/domain/value-objects/userEmail/userEmail.ts
@@ -1,6 +1,7 @@
-import { Result } from "./../../../../../shared/core/Result";
 import { ValueObject } from "../../../../../shared/domain/ValueObject";
-import validator from "validator"
+import { Result } from "./../../../../../shared/core/Result";
+import { Guard } from "../../../../../shared/core/Guard";
+import validator from "validator";
 
 export type UserEmailProps = {
   value: string;
@@ -16,7 +17,7 @@ export class UserEmail extends ValueObject<UserEmailProps> {
   }
 
   private static isValidEmail(email: string) {
-    return validator.isEmail(email)
+    return validator.isEmail(email);
   }
 
   private static formatEmail(email: string): string {
@@ -24,6 +25,10 @@ export class UserEmail extends ValueObject<UserEmailProps> {
   }
 
   public static create(email: string): Result<UserEmail> {
+    const userEmailResult = Guard.againstNullOrUndefined(email, "email");
+    if (userEmailResult.isFailure) {
+      return Result.fail<UserEmail>(userEmailResult.getErrorValue());
+    }
     if (!this.isValidEmail(email)) {
       return Result.fail<UserEmail>("Insira um email válido.");
     }
